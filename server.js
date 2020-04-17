06bdc65ce2e4d66d5a7c44cc6fc15ee227c8acbf
ycl310 <60243870+ycl310@users.noreply.github.com>
2020-04-16 20:38:46 -0700

    Minor Change
    Added process.env.PORT variable in the port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 const server = require('http').createServer(app)
 const io = require('socket.io').listen(server)
-server.listen(3000, '0.0.0.0')
+server.listen(process.env.PORT || 3000)
 
 const users = []
 
